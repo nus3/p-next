7d3b587fe6bcab3c43d75473967c41cf147a0bd2
yota-hada <hadayota33@gmail.com>
2021-01-19 19:08:40 +0900

    :sparkles: Add question form.

---
+++ src/components/organisms/forms/QuestionForm/index.stories.tsx
@@ -0,0 +1,17 @@
+import {
+  QuestionForm,
+  QuestionFormValues,
+} from 'components/organisms/forms/QuestionForm'
+
+export default {
+  title: 'organisms/forms/QuestionForm',
+}
+
+export const Default = (): JSX.Element => {
+  const onSubmit = (values: QuestionFormValues) => {
+    // eslint-disable-next-line
+    console.log(values)
+  }
+
+  return <QuestionForm onSubmit={onSubmit} />
+}
---
+++ src/components/organisms/forms/QuestionForm/index.tsx
@@ -0,0 +1,107 @@
+import { useEffect } from 'react'
+import {
+  FormProvider,
+  useFieldArray,
+  useForm,
+  useFormContext,
+} from 'react-hook-form'
+import styles from './style.module.scss'
+
+export type Question = {
+  text: string
+}
+
+export type QuestionFormValues = {
+  questions: Question[]
+}
+
+export type QuestionFormPresenterProps = {
+  onSubmit: (values: QuestionFormValues) => void
+}
+
+export const QuestionFormPresenter = ({
+  onSubmit,
+}: QuestionFormPresenterProps): JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    control,
+  } = useFormContext<QuestionFormValues>()
+  const { fields, append, remove } = useFieldArray<Question>({
+    control,
+    name: 'questions',
+  })
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles['question-list-wrap']}>
+        {fields.map((f, i) => (
+          <div key={f.id} className={styles['question-wrap']}>
+            <input
+              className={styles['input']}
+              name={`questions[${i}].text`}
+              ref={register()}
+              placeholder="text"
+            />
+            <button
+              className={styles['btn']}
+              type="button"
+              onClick={() => {
+                remove(i)
+              }}
+            >
+              削除
+            </button>
+          </div>
+        ))}
+        <button
+          className={styles['question-list-btn']}
+          onClick={() => {
+            append({ text: '' })
+          }}
+        >
+          質問追加
+        </button>
+      </div>
+      <button className={styles['submit-btn']} type="submit">
+        保存
+      </button>
+    </form>
+  )
+}
+
+export type QuestionFormContainerProps = {
+  onSubmit: (values: QuestionFormValues) => void
+  formValues?: QuestionFormValues
+}
+
+export const QuestionFormContainer = ({
+  onSubmit,
+  formValues,
+}: QuestionFormContainerProps): JSX.Element => {
+  const { reset } = useFormContext<QuestionFormValues>()
+
+  useEffect(() => {
+    reset(formValues)
+  }, [formValues])
+
+  return <QuestionFormPresenter onSubmit={onSubmit} />
+}
+
+export type QuestionFormProps = {
+  onSubmit: (values: QuestionFormValues) => void
+  formValues?: QuestionFormValues
+}
+
+export const QuestionForm = ({
+  onSubmit,
+  formValues,
+}: QuestionFormProps): JSX.Element => {
+  const methods = useForm<QuestionFormValues>()
+
+  return (
+    <FormProvider {...methods}>
+      <QuestionFormContainer onSubmit={onSubmit} formValues={formValues} />
+    </FormProvider>
+  )
+}
