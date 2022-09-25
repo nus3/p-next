c2c47f6421415802cf378d2bbd8d376c3736ad52
Yota Hada <nus3@YotanoMacBook-Pro.local>
2022-09-25 12:25:39 +0900

    :+1: add store2 page

--- src/pages/store2.tsx
+++ src/pages/store2.tsx
@@ -2,7 +2,7 @@ import { useUserStore } from 'hooks/store/useUserStore'
 import { NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
 
-const StorePage: NextPage = () => {
+const Store2Page: NextPage = () => {
   const userStore = useUserStore()
 
   const router = useRouter()
@@ -11,27 +11,9 @@ const StorePage: NextPage = () => {
     // eslint-disable-next-line
     console.log(userStore.user)
   }
-  const handleUpdate = () => {
-    // NOTE: apiから認証後にjwtを受け取る(_app.tsxでやる)
-    userStore.fetched(true)
 
-    userStore.update({
-      name: 'name',
-      age: 28,
-      email: 'email',
-      token: 'token',
-      history: [],
-    })
-  }
-  const handleReset = () => {
-    userStore.reset()
-  }
   const handleAddHistory = () => {
     userStore.addHistory('foo')
-  }
-
-  const handleMoveIndex = () => {
-    router.push('/redirect')
   }
 
   return (
@@ -39,12 +21,6 @@ const StorePage: NextPage = () => {
       <h1>storeの動作確認</h1>
       <button type="button" onClick={handleConfirm}>
         確認
-      </button>
-      <button type="button" onClick={handleUpdate}>
-        update
-      </button>
-      <button type="button" onClick={handleReset}>
-        reset
       </button>
       <button type="button" onClick={handleAddHistory}>
         addHistory
@@ -54,27 +30,16 @@ const StorePage: NextPage = () => {
       <p>{userStore.user.email}</p>
       <p>{userStore.user.token}</p>
       <p>ヒストリーの数：{userStore.user.history.length}</p>
-      <button type="button" onClick={handleMoveIndex}>
-        redirectページへ
-      </button>
       <button
         type="button"
         onClick={() => {
-          router.push('/redirect/context')
+          router.push('/store')
         }}
       >
-        contextへ
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          router.push('/store2')
-        }}
-      >
-        store2へ
+        storeへ
       </button>
     </div>
   )
 }
 
-export default StorePage
+export default Store2Page
